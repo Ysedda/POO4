36226ec59050c1985b51afefafb9d92619212968
Ysedda <arthur.baudoin@icloud.com>
2022-04-12 09:36:18 +0200

    questo

---
+++ Car.php
@@ -0,0 +1,88 @@
+<?php
+
+class Car
+{
+    private string $color;
+    private int $currentSpeed;
+    private int $nbSeats;
+    private int $nbWheels;
+    private string $energy;
+    private bool $hasParkBrake;
+
+    public function __construct(string $color, int $nbSeats, string $energy)
+    {
+        $this->color = $color;
+        $this->nbSeats = $nbSeats;
+        $this->energy = $energy;
+    }
+
+    public function forward(): string
+    {
+        $this->currentSpeed = 15;
+        return "Go !";
+    }
+
+    public function start(): string
+    {
+        return "I'm started";
+    }
+
+    public function brake(): string
+    {
+        $sentence = "";
+        while ($this->currentSpeed > 0) {
+            $this->currentSpeed--;
+            $sentence .= "Brake !!!";
+        }
+
+        $sentence .= "I'm stopped !";
+        return $sentence;
+    }
+
+    public function getCurrentSpeed(): int
+    {
+        return $this->currentSpeed;
+    }
+
+    public function setCurrentSpeed(int $currentSpeed): void
+    {
+        if ($currentSpeed >= 0) {
+            $this->currentSpeed = $currentSpeed;
+        }
+    }
+
+    public function getColor(): string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): void
+    {
+        $this->color = $color;
+    }
+
+    public function getNbSeats(): int
+    {
+        return $this->nbSeats;
+    }
+
+    public function setNbSeats(int $nbSeats): void
+    {
+        $this->nbSeats = $nbSeats;
+    }
+
+    public function getNbWheels(): int
+    {
+        return $this->nbWheels;
+    }
+
+    public function setNbWheels(int $nbWheels): void
+    {
+        $this->nbWheels = $nbWheels;
+    }
+
+    public function setParkBrake(bool $hasParkBrake): void
+    {
+        $this->hasParkBrake = $hasParkBrake;
+    }
+}
